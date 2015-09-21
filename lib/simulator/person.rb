a1a833a9eb70b5e7938fde670fbc982db7e46124
KTAtkinson <kaitlin.krouth@gmail.com>
2015-09-20 18:01:53 -0700

    Add attrs th person class and add methods

--- lib/simulator/person.rb
+++ lib/simulator/person.rb
@@ -1,9 +1,31 @@
 module PopulationGrowthSimulator
   class Person
-    def initialize(gender, age, health=100)
+    def initialize(gender, age, health=100, health_modifier=1, ability=100, ability_modifier=1)
       @gender = gender
       @age = age
       @health = health
+      @health_modifier = health_modifier
+      @ability = ability
+      @ability_modifier = ability_modifier
+    end
+    attr_reader :gender
+    
+    public
+    def fage(format)
+      age.each {|incrament, value| format.gsub!('%' + incrament, value)}
+      return format
+    end
+    
+    def age(days=30)
+      @age[days] += days
+      if @age[days] > 30
+        @age[months] += @age[days]/30
+        @age[days] %= 30
+      end
+      if @age[months] > 12
+        @age[years] += @age[monts]/12
+        @age[months] %= 12
+      end
     end
   end
 end
