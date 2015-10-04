27cf89386afb8c3ae5ad80dd6f4e500a912aac60
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 15:26:38 -0700

    Add spec to PopulationGrowthSimulator module

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
-describe Person do
-  person  = Person('female', {'years'=>15, 'months'=>10, 'days'=>10})
-  
-  describe 'age' do
-    it 'increases the persons age by the given number of days' do
-      age_by = 30
-      new_age = person.age? + (86400000 * age_by)
-      person.age(age_by)
-      expect(person.age?).to equal(new_age)
+module PopulationGrowthSimulator
+
+  describe Person do
+    person  = Person('female', {'years'=>15, 'months'=>10, 'days'=>10})
+    
+    describe 'age' do
+      it 'increases the persons age by the given number of days' do
+        age_by = 30
+        new_age = person.age? + (86400000 * age_by)
+        person.age(age_by)
+        expect(person.age?).to equal(new_age)
+      end
     end
   end
 end
