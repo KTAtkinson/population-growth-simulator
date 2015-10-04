e1e97d83208da34b84691305f4131946c985b495
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 15:52:47 -0700

    Set Gender class instance vars

--- lib/simulator/constants.rb
+++ lib/simulator/constants.rb
@@ -1,13 +1,12 @@
 module PopulationGrowthSimulator
   class Gender
-    @@FEMALE = 0
-    @@MALE = 1
-    
     attr_reader :FEMALE
     attr_reader :MALE
+    
+    @@FEMALE = 0
+    @@MALE = 1
   end
   
   MILLI_PER_DAY = 86400000
   MILLI_PER_MONTH = 2.62974e9
   MILLI_PER_YEAR = 3.156e+10
-end
