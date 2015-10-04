585a3fe9ded1ae4a97cc9fb71c81ed74669739e6
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 14:31:04 -0700

    adds first test for person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Person' do
+  person  = Person('female', {'years':15, 'months':10, 'days':10})
+  
+  describe 'age' do
+    it 'increases the persons age by the given number of days' do
+      age_by = 30
+      new_age = person.age? + (86400000 * age_by)
+      person.age(age_by)
+      expect(person.age?).to equal(new_age)
+    end
+  end
+end
+  
