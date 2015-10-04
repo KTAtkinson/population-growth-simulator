036ae4245e13a8465fc01134655492094545eb14
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 15:16:21 -0700

    fix spelling error

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -1,6 +1,6 @@
 require "simulator/simulator.rb"
 require "simulator/person.rb"
-require "simulator/consants.rb"
+require "simulator/constants.rb"
 
 module DigestEmail
   def self.parse(digest_hash)
