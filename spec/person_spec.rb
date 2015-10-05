ecf727d0825b98522935ca0bd18549d411a55fbf
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 20:40:01 -0700

    Adds more advanced test
    * tests for increase_age
    * tests for get_age_tuple

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -13,11 +13,24 @@ module PopulationGrowthSimulator
         person.increase_age(days=age_by)
         expect(person.age).to eq(new_age)
       end
+      it 'increases the persons age by the given number of years' do
+        age_by = 30
+        new_age = person.age + (MILLI_PER_YEAR*age_by)
+        person.increatse_age(years=age_by)
+        expect(person.age).to eq(new_age)
+      end
     end
     
     describe 'get_age_tuple' do
       it 'returns tuple containing years, month, age' do
         expect(person.get_age_tuple).to eq([1, 1, 1])
+      end
+      it 'returns correct tuple when age is high' do
+        years = 105
+        months = 10
+        days = 28
+        person.increase_age(days=days, months=months, years=years)
+        expect(person.get_age_tuple).to eq([years+1, months+1, days+1])
       end
     end
   end
