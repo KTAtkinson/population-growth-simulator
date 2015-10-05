b486f43d2eb816853348b32932c90418ec98243b
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 19:03:31 -0700

    update MILLI_PER_DAY to float

--- lib/simulator/constants.rb
+++ lib/simulator/constants.rb
@@ -5,7 +5,7 @@ module PopulationGrowthSimulator
     MALE = 1
   end
   
-  MILLI_PER_DAY = 86400000
+  MILLI_PER_DAY = 8.64e8
   MILLI_PER_MONTH = 2.62974e9
-  MILLI_PER_YEAR = 3.156e+10
+  MILLI_PER_YEAR = 3.156e10
 end
