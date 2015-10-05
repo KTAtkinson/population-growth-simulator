55719bf8e915740a68cf36261ca7ac3ab8fef00f
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-04 21:03:56 -0700

    Update tests to recreate a person for every test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 module PopulationGrowthSimulator
 
   describe Person do
-    let(:age) {MILLI_PER_YEAR+MILLI_PER_MONTH+MILLI_PER_DAY}
-    let(:person) {Person.new(Gender::FEMALE, age)}
     
     describe 'increase_age' do
       it 'increases the persons age by the given number of days' do
+        person = Person.new(Gender::FEMALE, 0)
         age_by = 30
-        new_age = person.age + (MILLI_PER_DAY * age_by)
+        new_age = MILLI_PER_DAY * age_by
         person.increase_age(days=age_by)
         expect(person.age).to eq(new_age)
       end
       it 'increases the persons age by the given number of years' do
+        person = Person.new(Gender::FEMALE, 0)
         age_by = 30
-        new_age = person.age + (MILLI_PER_YEAR*age_by)
+        new_age = MILLI_PER_YEAR*age_by
         person.increase_age(years=age_by)
         expect(person.age).to eq(new_age)
       end
@@ -23,6 +23,8 @@ module PopulationGrowthSimulator
     
     describe 'get_age_tuple' do
       it 'returns tuple containing years, month, age' do
+        age = MILLI_PER_YEAR + MILLI_PER_MONTH + MILLI_PER_DAY
+        person = Person.new(Gender::FEMALE, age)
         expect(person.get_age_tuple).to eq([1, 1, 1])
       end
       it 'returns correct tuple when age is high' do
