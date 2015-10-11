55a6eaed8bdeec931d1b3fb2b28e2a8f6cc3baea
KTAtkinson <kaitlin.krouth@gmail.com>
2015-10-11 15:19:50 -0700

    Added/edits tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,18 +6,38 @@ module PopulationGrowthSimulator
     
     describe 'increase_age' do
       it 'increases the persons age by the given number of days' do
-        person = Person.new(Gender::FEMALE, 0)
+        starting_age = 1000
+        person = Person.new(Gender::FEMALE, starting_age)
         age_by = 30
-        new_age = MILLI_PER_DAY * age_by
+        new_age = starting_age + MILLI_PER_DAY * age_by
         person.increase_age(days=age_by)
         expect(person.age).to eq(new_age)
       end
       it 'increases the persons age by the given number of years' do
-        person = Person.new(Gender::FEMALE, 0)
+        starting_age = 1000
+        person = Person.new(Gender::FEMALE, starting_age)
         age_by = 30
-        new_age = MILLI_PER_YEAR*age_by
+        new_age = starting_age + MILLI_PER_YEAR*age_by
         person.increase_age(days=0, months=0, years=age_by)
         expect(person.age).to eq(new_age)
+      end
+      if 'increases the persons age by the given number of months' do
+        starting_age = 1000
+        person = Person.new(Gender::FEMALE, starting_age)
+        age_by = 30
+        new_age = starting_age + MILLI_PER_MONTH * age_by
+        person.increase_age(days=0, months=age_by)
+        expect(person.age).is eq(new_age)
+      end
+      if "increases the persons age by given days/months/years" do
+        starting_age = 1000
+        person = Person.new(Gender::FEMALE, starting_age)
+        days = 12
+        months = 8
+        years = 10
+        new_age = starting_age + MILLI_PER_DAY*days + MILLI_PER_MONTH*months + MILLI_PER_YEAR*pears
+        person.increase_age(days=days, months=months, years=years)
+        expect(person.age).is eq(new_age)
       end
     end
     
